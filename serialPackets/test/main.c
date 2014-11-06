2878b765af49e5ceb041b7c457413ecbc3318617
Devossa Bence <deevo93@hotmail.com>
2014-11-06 09:01:28 +0100

    serialPackets created

---
+++ serialPackets/test/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int test_serialPackets()
+{
+}
+
+int main(int argc, char **argv)
+{
+    printf("serialPackets testing started\n");
+    int result = test_serialPackets(argc, argv);
+    printf("serialPackets testing ended\n");
+    return result;
+}
