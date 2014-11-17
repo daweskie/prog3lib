59a0ef616f16284177c15643ea868bd7983890f0
Molnár Gergely <thenextwolf@gmail.com>
2014-11-17 16:52:20 +0100

    Finish with Header file

--- fileWrite/include/fileWrite.h
+++ fileWrite/include/fileWrite.h
@@ -32,9 +32,29 @@ char fw_fileWriteDefault(const char *pathname, const char *buffer);
  @param buffer data for write (What?)
  @param nexists 0 if file or direction is not exits and you don't create it; 1 if you want create it
  @param content 0 if you would like re-write file; 1 if you would like to append a new data to the file
- @return return 1 if eweryting is awesome, return 0 if error, return 2 if created new file or direction
+ @return return 1 if everything is awesome, return 0 if error, return 2 if created new file or direction
 */
 char fw_fileWrite(const char *pathname, const char *buffer, const char nexists, const char content);
+
+
+/**
+ Create file and write to TEMP/fw_{date}/{time}.txt
+
+ @param pathname filename
+ @param buffer data for write (What?)
+ @return return 1 if eweryting is awesome, return 0 if error
+*/
+char fw_fileWriteTempDefault(const char *buffer);
+
+
+/**
+ File Writer in TEMP: write buffer to file in temp, if not exits file or direction then the function create it
+
+ @param pathname filename
+ @param buffer data for write (What?)
+ @return return 1 if eweryting is awesome, return 0 if error, return 2 if created new file or direction
+*/
+char fw_fileWriteTemp(const char *filename, const char *buffer);
 
 
 /**
@@ -53,3 +73,39 @@ char fw_exists(const char *pathname);
  @return return 1 if it is file, 2 if it is direction, 0 if not exists
 */
 char fw_fod(const char *pathname);
+
+
+/**
+ Delete File or Direction
+
+ @param pathname path and filename
+ @return return 1 if delete file, 2 if delete direction, 0 if error
+*/
+char fw_delete(const char *pathname);
+
+
+/**
+ Read Only info
+
+ @param pathname path and filename
+ @return return 1 if read only or 0 if not exists
+*/
+char fw_readonly(const char *pathname);
+
+
+/**
+ Can write info
+
+ @param pathname path and filename
+ @return return 1 if can write, 0 if read only or not exists
+*/
+char fw_canwrite(const char *pathname);
+
+
+/**
+ Create Direction
+
+ @param pathname path and filename
+ @return return 1 if everything is awesome, 0 error
+*/
+char fw_mkdir(const char *pathname);
