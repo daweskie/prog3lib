0dbbc892da64d527d73dcf8bacbd92daede659d2
Molnár Gergely <thenextwolf@gmail.com>
2014-11-17 16:22:31 +0100

    continous programing #1

--- fileWrite/include/fileWrite.h
+++ fileWrite/include/fileWrite.h
@@ -22,15 +22,34 @@ fileWrite library
  @param buffer data for write (What?)
  @return return 1 if eweryting is awesome, return 0 if error, return 2 if created new file or direction
 */
-char fw_filewrite_default(const char *pathname, const char *buffer);
+char fw_fileWriteDefault(const char *pathname, const char *buffer);
+
 
 /**
  File Writer: write buffer to file, if not exits file or direction then the function create it
 
  @param pathname path and filename (Where?)
  @param buffer data for write (What?)
- @param nexits 0 if file or direction is not exits and you don't create it; 1 if you want create it
+ @param nexists 0 if file or direction is not exits and you don't create it; 1 if you want create it
  @param content 0 if you would like re-write file; 1 if you would like to append a new data to the file
  @return return 1 if eweryting is awesome, return 0 if error, return 2 if created new file or direction
 */
-char fw_filewrite(const char *pathname, const char *buffer, const char nexits, const char content);
+char fw_fileWrite(const char *pathname, const char *buffer, const char nexists, const char content);
+
+
+/**
+ File Exists info - Question: be or not be?
+
+ @param pathname path and filename
+ @return return 1 if file is exits or 0 if not exits
+*/
+char fw_exists(const char *pathname);
+
+
+/**
+ File or Direction info
+
+ @param pathname path and filename
+ @return return 1 if it is file, 2 if it is direction, 0 if not exists
+*/
+char fw_fod(const char *pathname);
