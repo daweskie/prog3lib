5363ed83b76c0b708befde73ae49fca7350ca8c2
rebreg <rebreg@B950.com>
2014-11-19 16:18:59 -0500

    socketclient

---
+++ socketclient/test/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int test_socketclient()
+{
+}
+
+int main(int argc, char **argv)
+{
+    printf("socketclient testing started\n");
+    int result = test_socketclient(argc, argv);
+    printf("socketclient testing ended\n");
+    return result;
+}
